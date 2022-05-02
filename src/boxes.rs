f19e1f593c82eda7dd52606e88fbd33208e3d87c
WistfulHopes <kp72002@gmail.com>
2022-05-02 13:58:10 -0700

    updated to support all box types

--- src/boxes.rs
+++ src/boxes.rs
@@ -32,6 +32,56 @@ impl Default for Box {
     }
 }
 
+#[derive(Copy, Clone)]
+enum BoxType {
+    Hurtbox = 0,
+    Hitbox = 1,
+    ExPoint = 2,
+    ExRect = 3,
+    ExVector = 4,
+    Push = 5,
+    TempCenter = 6,
+    Neck = 7,
+    Abdominal = 8,
+    AttackVsPush = 9,
+    SpGuard = 10,
+    RLeg = 11,
+    LLeg = 12,
+    Private0 = 13,
+    Private1 = 14,
+    Private2 = 15,
+    Private3 = 16,
+    ExtendJon = 17,
+}
+
+impl TryFrom<u32> for BoxType {
+    type Error = ();
+
+    fn try_from(v: u32) -> Result<Self, Self::Error> {
+        match v {
+            x if x == BoxType::Hurtbox as u32 => Ok(BoxType::Hurtbox),
+            x if x == BoxType::Hitbox as u32 => Ok(BoxType::Hitbox),
+            x if x == BoxType::ExPoint as u32 => Ok(BoxType::ExPoint),
+            x if x == BoxType::ExRect as u32 => Ok(BoxType::ExRect),
+            x if x == BoxType::ExVector as u32 => Ok(BoxType::ExVector),
+            x if x == BoxType::Push as u32 => Ok(BoxType::Push),
+            x if x == BoxType::TempCenter as u32 => Ok(BoxType::TempCenter),
+            x if x == BoxType::Neck as u32 => Ok(BoxType::Neck),
+            x if x == BoxType::Abdominal as u32 => Ok(BoxType::Abdominal),
+            x if x == BoxType::AttackVsPush as u32 => Ok(BoxType::AttackVsPush),
+            x if x == BoxType::SpGuard as u32 => Ok(BoxType::SpGuard),
+            x if x == BoxType::RLeg as u32 => Ok(BoxType::RLeg),
+            x if x == BoxType::LLeg as u32 => Ok(BoxType::LLeg),
+            x if x == BoxType::Private0 as u32 => Ok(BoxType::Private0),
+            x if x == BoxType::Private1 as u32 => Ok(BoxType::Private1),
+            x if x == BoxType::Private2 as u32 => Ok(BoxType::Private2),
+            x if x == BoxType::Private3 as u32 => Ok(BoxType::Private3),
+            x if x == BoxType::ExtendJon as u32 => Ok(BoxType::ExtendJon),
+            _ => Err(()),
+        }
+    }
+}
+
 pub struct BoxesWindow {
     path: PathBuf,
     pub jonbins: BTreeMap<String, GGSTJonBin>,
@@ -129,43 +179,45 @@ Right click to reset to the original position.");
 
     fn box_list(&mut self, ui: &mut egui::Ui) {
         let jonb = self.jonbins.get(&self.selected).unwrap();
-        let mut hurt_index = 0;
-        let mut hit_index = 0;
         ui.horizontal(|ui| {
             ComboBox::from_label("Box list")
             .selected_text(format!("{} #{}", self.boxtype, self.box_index))
             .width(150.0)
             .show_ui(ui, |ui| {
                 for boxgroup in &jonb.boxes {
-                    for hitbox in boxgroup {
-                        if hitbox.kind == 0 {
-                            hurt_index += 1;
-                            if ui.selectable_label(true, format!("Hurtbox #{}", hurt_index))
-                            .clicked()
-                            {
-                                self.box_index = hurt_index;
-                                self.boxtype = format!("Hurtbox");
-                                self.box_info.x = format!("{}", hitbox.rect.x_offset);
-                                self.box_info.y = format!("{}", hitbox.rect.y_offset);
-                                self.box_info.w = format!("{}", hitbox.rect.width);
-                                self.box_info.h = format!("{}", hitbox.rect.height);
-                                self.current_box = Some(*hitbox);
-                            };
-                        }
-                        if hitbox.kind == 1 {
-                            hit_index += 1;
-                            if ui.selectable_label(true, format!("Hitbox #{}", hit_index))
-                            .clicked()
-                            {
-                                self.box_index = hit_index;
-                                self.boxtype = format!("Hitbox");
-                                self.box_info.x = format!("{}", hitbox.rect.x_offset);
-                                self.box_info.y = format!("{}", hitbox.rect.y_offset);
-                                self.box_info.w = format!("{}", hitbox.rect.width);
-                                self.box_info.h = format!("{}", hitbox.rect.height);
-                                self.current_box = Some(*hitbox);
-                            };    
-                        }    
+                    for (index, hitbox) in boxgroup.iter().enumerate() {
+                        let kind = match hitbox.kind.try_into(){
+                            Ok(BoxType::Hurtbox) => "Hurtbox",
+                            Ok(BoxType::Hitbox) => "Hitbox",
+                            Ok(BoxType::ExPoint) => "ExPoint",
+                            Ok(BoxType::ExRect) => "ExRect",
+                            Ok(BoxType::ExVector) => "ExVector",
+                            Ok(BoxType::Push) => "Push",
+                            Ok(BoxType::TempCenter) => "TempCenter",
+                            Ok(BoxType::Neck) => "Neck",
+                            Ok(BoxType::Abdominal) => "Abdominal",
+                            Ok(BoxType::AttackVsPush) => "AttackVsPush",
+                            Ok(BoxType::SpGuard) => "SpGuard",
+                            Ok(BoxType::RLeg) => "RLeg",
+                            Ok(BoxType::LLeg) => "LLeg",
+                            Ok(BoxType::Private0) => "Private0",
+                            Ok(BoxType::Private1) => "Private1",
+                            Ok(BoxType::Private2) => "Private2",
+                            Ok(BoxType::Private3) => "Private3",
+                            Ok(BoxType::ExtendJon) => "ExtendJon",
+                            Err(_) => ""
+                        };
+                        if ui.selectable_label(true, format!("{} #{}", kind, index))
+                        .clicked()
+                        {
+                            self.box_index = index as u32;
+                            self.boxtype = kind.to_string();
+                            self.box_info.x = format!("{}", hitbox.rect.x_offset);
+                            self.box_info.y = format!("{}", hitbox.rect.y_offset);
+                            self.box_info.w = format!("{}", hitbox.rect.width);
+                            self.box_info.h = format!("{}", hitbox.rect.height);
+                            self.current_box = Some(*hitbox);
+                        };
                     }
                 }
             });
@@ -175,21 +227,8 @@ Right click to reset to the original position.");
     fn box_edit(&mut self, ui: &mut egui::Ui, hitbox: Option<HitBox>) {
         match hitbox {
             None => {
-                ui.vertical(|ui| {
-                    ui.label("X Offset");
-                    ui.text_edit_singleline(&mut "0.0");
-                });
-                ui.vertical(|ui| {
-                    ui.label("Y Offset");
-                    ui.text_edit_singleline(&mut "0.0");
-                });
-                ui.vertical(|ui| {
-                    ui.label("Width");
-                    ui.text_edit_singleline(&mut "0.0");
-                });
-                ui.vertical(|ui| {
-                    ui.label("Height");
-                    ui.text_edit_singleline(&mut "0.0");
+                ui.horizontal(|ui| {
+                    ui.label("Please select a box!");
                 });
             },
             Some(mut hitbox) => {
@@ -258,48 +297,82 @@ Right click to reset to the original position.");
             self.offset_x = 640.0;
             self.offset_y = 802.0;
         }
-
-        let mut hurt_index = 0;
-        let mut hit_index = 0;
-
+        
         for boxgroup in &mut jonb.boxes {
-            for mut hitbox in boxgroup {
-                if hitbox.kind == 0 {
-                    hurt_index += 1;
-                    if self.box_index == hurt_index && self.boxtype == format!("Hurtbox")
-                    {
-                        hitbox.rect.x_offset = self.current_box.unwrap().rect.x_offset;
-                        hitbox.rect.y_offset = self.current_box.unwrap().rect.y_offset;
-                        hitbox.rect.width = self.current_box.unwrap().rect.width;
-                        hitbox.rect.height = self.current_box.unwrap().rect.height;
-                    }
-                    painter.rect_stroke(
-                        Rect { min: Pos2{x: (hitbox.rect.x_offset + self.offset_x ), 
-                            y: (hitbox.rect.y_offset + self.offset_y)}, 
-                            max: Pos2{x: (hitbox.rect.x_offset + hitbox.rect.width + self.offset_x ), 
-                            y: (hitbox.rect.y_offset + hitbox.rect.height + self.offset_y)} },
-                        0.0, 
-                        Stroke{width: 3.0, color: Color32::GREEN},
-                    );
+            for (index, hitbox) in boxgroup.iter_mut().enumerate() {
+                let mut color = Color32::GREEN;
+                let kind = match hitbox.kind.try_into(){
+                    Ok(BoxType::Hurtbox) => {
+                        color = Color32::GREEN;
+                        "Hurtbox"},
+                    Ok(BoxType::Hitbox) => {
+                        color = Color32::RED;
+                        "Hitbox"},
+                    Ok(BoxType::ExPoint) => {
+                        color = Color32::BLUE;
+                        "Hitbox"},
+                    Ok(BoxType::ExRect) => {
+                        color = Color32::GOLD;
+                        "ExRect"},
+                    Ok(BoxType::ExVector) => {
+                        color = Color32::YELLOW;
+                        "ExVector"},
+                    Ok(BoxType::Push) => {
+                        color = Color32::DARK_BLUE;
+                        "Push"},
+                    Ok(BoxType::TempCenter) => {
+                        color = Color32::LIGHT_GREEN;
+                        "TempCenter"},
+                    Ok(BoxType::Neck) => {
+                        color = Color32::LIGHT_RED;
+                        "Neck"},
+                    Ok(BoxType::Abdominal) => {
+                        color = Color32::LIGHT_BLUE;
+                        "Abdominal"},
+                    Ok(BoxType::AttackVsPush) => {
+                        color = Color32::LIGHT_YELLOW;
+                        "AttackVsPush"},
+                    Ok(BoxType::SpGuard) => {
+                        color = Color32::DEBUG_COLOR;
+                        "SpGuard"},
+                    Ok(BoxType::RLeg) => {
+                        color = Color32::KHAKI;
+                        "RLeg"},
+                    Ok(BoxType::LLeg) => {
+                        color = Color32::BROWN;
+                        "LLeg"},
+                    Ok(BoxType::Private0) => {
+                        color = Color32::GRAY;
+                        "LLeg"},
+                    Ok(BoxType::Private1) => {
+                        color = Color32::BLACK;
+                        "LLeg"},
+                    Ok(BoxType::Private2) => {
+                        color = Color32::LIGHT_GRAY;
+                        "LLeg"},
+                    Ok(BoxType::Private3) => {
+                        color = Color32::DARK_GRAY;
+                        "SpGuard"},
+                    Ok(BoxType::ExtendJon)  => {
+                        color = Color32::DARK_RED;
+                        "ExtendJon"},
+                    Err(_) => ""
+                };
+                if self.box_index == index as u32 && self.boxtype == kind
+                {
+                    hitbox.rect.x_offset = self.current_box.unwrap().rect.x_offset;
+                    hitbox.rect.y_offset = self.current_box.unwrap().rect.y_offset;
+                    hitbox.rect.width = self.current_box.unwrap().rect.width;
+                    hitbox.rect.height = self.current_box.unwrap().rect.height;
                 }
-                if hitbox.kind == 1 {
-                    hit_index += 1;
-                    if self.box_index == hit_index && self.boxtype == format!("Hitbox")
-                    {
-                        hitbox.rect.x_offset = self.current_box.unwrap().rect.x_offset;
-                        hitbox.rect.y_offset = self.current_box.unwrap().rect.y_offset;
-                        hitbox.rect.width = self.current_box.unwrap().rect.width;
-                        hitbox.rect.height = self.current_box.unwrap().rect.height;
-                    }
-                    painter.rect_stroke(
-                        Rect { min: Pos2{x: (hitbox.rect.x_offset + self.offset_x ), 
-                            y: (hitbox.rect.y_offset + self.offset_y)}, 
-                            max: Pos2{x: (hitbox.rect.x_offset + hitbox.rect.width + self.offset_x ), 
-                            y: (hitbox.rect.y_offset + hitbox.rect.height + self.offset_y)} },
-                        0.0, 
-                        Stroke{width: 3.0, color: Color32::RED},
-                    );
-                }    
+                painter.rect_stroke(
+                    Rect { min: Pos2{x: (hitbox.rect.x_offset + self.offset_x - 1.5), 
+                        y: (hitbox.rect.y_offset + self.offset_y - 1.5)}, 
+                        max: Pos2{x: (hitbox.rect.x_offset + hitbox.rect.width + self.offset_x + 1.5 ), 
+                        y: (hitbox.rect.y_offset + hitbox.rect.height + self.offset_y + 1.5)} },
+                    0.0, 
+                    Stroke{width: 3.0, color},
+                );
             }
         }
         response
