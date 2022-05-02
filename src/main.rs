a05620f2147ef710f6c4013d724bc73e2f690713
WistfulHopes <kp72002@gmail.com>
2022-05-02 13:58:34 -0700

    version update

--- src/main.rs
+++ src/main.rs
@@ -27,7 +27,7 @@ fn main() -> AResult<()> {
         ..Default::default()
     };
     eframe::run_native(
-        "GGST Collision Editor Rust v3.3",
+        "GGST Collision Editor Rust v3.4",
         options,
         Box::new(|_cc| Box::new(MyApp::default())),
     );
